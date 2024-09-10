785147fb0679e471d2cc60d2d64a6363b1fedd68
Athulya Esther <126341347+athulyaesther777@users.noreply.github.com>
2024-09-10 15:32:38 +0530

    Add files via upload

---
+++ hangman/hangman_words.py
@@ -0,0 +1,113 @@
+word_list = [
+    "apple", "ball", "cat", "dog", "elephant", "frog", "giraffe", "hat", "ice",
+    "jacket", "kite", "lion", "monkey", "nut", "octopus", "penguin", "quilt",
+    "rabbit", "snake", "tree", "umbrella", "violin", "whale", "xylophone",
+    "yacht", "zebra", "airplane", "book", "candle", "dolphin", "eggplant",
+    "fish", "grape", "house", "jungle", "kangaroo", "lemon", "mango", "night",
+    "orange", "puzzle", "queen", "rose", "star", "tiger", "ufo", "vase",
+    "waterfall", "xenon", "yogurt", "zoo", "avocado", "bicycle", "computer",
+    "diamond", "elephant", "fountain", "guitar", "honey", "igloo", "jackfruit",
+    "keyboard", "lamp", "moon", "ninja", "ocean", "parrot", "quasar", "robot",
+    "sunflower", "treehouse", "unicorn", "violet", "windmill", "xylophone",
+    "yarn", "zeppelin", "antelope", "blueberry", "carousel", "dolphin",
+    "envelope", "feather", "gorilla", "hammock", "insect", "jellyfish",
+    "koala", "lighthouse", "marshmallow", "notebook", "orchid", "panda",
+    "quince", "reindeer", "squirrel", "toucan", "ukulele", "volcano",
+    "whiskers", "xenopus", "yak", "zinnia",'Sun', 'Moon', 'Mars', 'Jupiter',
+    'Saturn', 'Neptune', 'Venus', 'Mercury',
+    'Earth', 'Pluto', 'Comet', 'Asteroid', 'Star', 'Galaxy', 'Nebula', 'Cosmos',
+    'Andromeda', 'Blackhole', 'Universe', 'Orion', 'Sirius', 'Titan', 'Europa',
+    'Ganymede', 'Callisto', 'Phobos', 'Deimos', 'Io', 'Triton', 'Hydra', 'Vulcan',
+    'Apollo', 'Zeus', 'Poseidon', 'Athena', 'Artemis', 'Hermes', 'Demeter', 'Hera',
+    'Hades', 'Persephone', 'Ares', 'Hephaestus', 'Hercules', 'Pandora', 'Oberon',
+    'Miranda', 'Titania', 'Rhea', 'Dione', 'Ceres', 'Eris', 'Sedna', 'Vesta',
+    'Kuiper', 'Belt', 'Oort', 'Cloud', 'Quasar', 'Pulsar', 'Meteor', 'Aurora',
+    'Pegasus', 'Phoenix', 'Lynx', 'Orca', 'Kraken', 'Gorgon', 'Cyclops', 'Medusa',
+    'Chimera', 'Sphinx', 'Minotaur', 'Griffin', 'Centaur', 'Harpy', 'Basilisk',
+    'Mermaid', 'Nymph', 'Dragon', 'Pixie', 'Elf', 'Dwarf', 'Fairy'
+]
+stages = [
+    '''
+     _____________
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |     _/ \_
+     |
+    _|___
+    ''',
+    '''
+     _____________
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      _/ 
+     |
+    _|___
+    ''',
+    '''
+     _____________
+     |/      |
+     |      (_)
+     |      \|/
+     |       |
+     |      
+     |
+    _|___
+    ''',
+    '''
+     _____________
+     |/      |
+     |      (_)
+     |      \|
+     |       
+     |      
+     |
+    _|___
+    ''',
+    '''
+     _____________
+     |/      |
+     |      (_)
+     |       |
+     |       
+     |      
+     |
+    _|___
+    ''',
+    '''
+     _____________
+     |/      |
+     |      (_)
+     |      
+     |       
+     |      
+     |
+    _|___
+    ''',
+    '''
+     _____________
+     |/      |
+     |      
+     |      
+     |       
+     |      
+     |
+    _|___
+    '''
+]
+
+logo = '''
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/                    
+'''
+
+
